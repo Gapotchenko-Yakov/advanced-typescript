65a34847b18d9aa06c40b18a214fb23723fe200c
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-12 18:21:24 +0300

    Recreating the Pick type

--- src/advanced-typescript/mapped-types.ts
+++ src/advanced-typescript/mapped-types.ts
@@ -8,4 +8,10 @@ type MyMappedType2<T extends string | number | symbol> = {
   [P in keyof MyMappedType<T>]: string | null;
 };
 
-let type: MyMappedType2<Properties>;
+let MyNewType: MyMappedType2<Properties>;
+
+type MyPick<T, Properties extends keyof T> = {
+  [P in Properties]: T[P];
+};
+
+type MyNewType2 = MyPick<{ a: "structure of a"; b: "structure of b" }, "a">;
