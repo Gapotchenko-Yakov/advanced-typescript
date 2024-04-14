7f95432b6e7f7d333f347f208c7c184f9227b9f7
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-14 17:44:52 +0300

    Comparing functions demo.

--- src/advanced-typescript/type-compatibility.ts
+++ src/advanced-typescript/type-compatibility.ts
@@ -26,3 +26,14 @@ class Accountant {
 let employee: EmployeeSuper = new Accountant("John");
 
 // employee.ruleMethod;
+let f1 = (a: string, b: number, c: boolean) => 0;
+let f2 = (arg1: string) => 0;
+
+// f1 = f2 // OK
+// f2 = f1 // ERROR
+
+let f1Return = (a: string) => ({ name: "Jack", age: 30 });
+let f2Return = (a: string) => ({ name: "Jack" });
+
+// f1Return = f2Return; // ERROR lacks an 'age' property
+// f2Return = f1Return; // OK
